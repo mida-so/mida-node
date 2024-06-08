1f935eb5454e020e2416635adacb32bebb63b785
Donald Ng <donaldyann@gmail.com>
2024-06-08 11:48:59 +0100

    Added event attribute

--- README.md
+++ README.md
@@ -1,95 +1,116 @@
-# Mida.so - Server-side A/B Testing
-
-This is a server-side A/B testing code that allows you to integrate with the Mida A/B testing platform. The code is written in JavaScript and can be used in a Node.js environment.
-
+# Mida.so - Server-side A/B Testing and Feature Flags
+This is a server-side A/B testing and feature flags code that allows you to integrate with the Mida platform. The code is written in JavaScript and can be used in a Node.js environment.
 ## Prerequisites
-
 Before using this code, make sure you have the following set up:
-
 - Node.js installed on your machine
 - A Mida.so account with project and experiment key
-
 ## Installation
-
 1. Install the necessary dependencies by running the following command:
-
 ```
 npm install mida-node
 ```
-
 ## Usage
-
-To use the server-side A/B testing code, follow these steps:
-
+To use the server-side A/B testing and feature flags code, follow these steps:
 1. Import the `Mida` class into your code:
-
 ```javascript
 const Mida = require('mida-node');
 ```
-
 2. Create an instance of the `Mida` class by providing your Mida project key:
-
 ```javascript
 const mida = new Mida('YOUR_PROJECT_KEY');
 ```
-
+### A/B Testing
 3. Use the `getExperiment` method to retrieve the current version of an experiment for a user. You need to provide the experiment key and the distinct ID of the user:
-
 ```javascript
 const experimentKey = 'EXPERIMENT_KEY';
 const distinctId = 'USER_DISTINCT_ID';
-
 const version = await mida.getExperiment(experimentKey, distinctId);
-
 if (version === 'Control') {
-    // Handle Control logic
+// Handle Control logic
 }
-
 if (version === 'Variant 1') {
-    // Handle Variant 1 logic
+// Handle Variant 1 logic
 }
-
 // Depending on how many variants you have created
 if (version === 'Variant 2') {
-    // Handle Variant 2 logic
+// Handle Variant 2 logic
 }
-
 ```
-
 4. Use the `setEvent` method to log an event for a user. You need to provide the event name and the distinct ID of the user:
-
 ```javascript
 const eventName = 'EVENT_NAME';
 const distinctId = 'USER_DISTINCT_ID';
-
-await mida.setEvent(eventName, distinctId)
+await mida.setEvent(eventName, distinctId);
 ```
 
+For revenue tracking, you can use the `setEvent` method with the event name as 'Purchase' and include additional attributes such as revenue, quantity, and currency. Here's an example:
+
+```javascript
+const eventName = 'Purchase';
+const distinctId = 'USER_DISTINCT_ID';
+const properties = {
+  revenue: 99.99,
+  quantity: 1,
+  currency: 'USD'
+};
+await mida.setEvent(eventName, distinctId, properties);
+```
+
+In this example, the `setEvent` method is called with the event name 'Purchase' and the `distinctId` of the user. The `properties` object contains the additional attributes related to the purchase event:
+- `revenue`: The total revenue amount of the purchase (e.g., 99.99).
+- `quantity`: The quantity of items purchased (e.g., 1).
+- `currency`: The currency of the revenue amount (e.g., 'USD').
+
+By including these attributes, you can track revenue-related information along with the purchase event.
+
+### User Attributes
+5. Use the `setAttribute` method to set user attributes for a specific user. You need to provide the distinct ID of the user and an object containing the attribute key-value pairs:
+```javascript
+const distinctId = 'USER_DISTINCT_ID';
+const attributes = {
+gender: 'male',
+company_name: 'Apple Inc'
+};
+await mida.setAttribute(distinctId, attributes);
+```
+### Feature Flags
+6. Use the `isFeatureEnabled` method to check if a feature flag is enabled for the current user:
+```javascript
+const featureFlagKey = 'FEATURE_FLAG_KEY';
+const isEnabled = await mida.isFeatureEnabled(featureFlagKey);
+if (isEnabled) {
+// Feature flag is enabled, perform corresponding actions
+} else {
+// Feature flag is disabled, perform alternative actions
+}
+```
+7. Use the `onFeatureFlags` method to reload the feature flags for the current user:
+```javascript
+await mida.onFeatureFlags();
+```
 ## API Reference
-
 ### `Mida(projectKey, options)`
-
 - `projectKey`: (required) Your Mida project key.
 - `options`: (optional) An object of additional options.
-
 ### `getExperiment(experimentKey, distinctId)`
-
 - `experimentKey`: (required) The key of the experiment you want to get the version of.
 - `distinctId`: (required) The distinct ID of the user.
-
 Returns a Promise that resolves to the version of the experiment.
-
 ### `setEvent(eventName, distinctId)`
-
 - `eventName`: (required) The name of the event you want to log.
 - `distinctId`: (required) The distinct ID of the user.
-
 Returns a Promise that resolves when the event is successfully logged.
-
+### `setAttribute(distinctId, properties)`
+- `distinctId`: (required) The distinct ID of the user.
+- `properties`: (required) An object containing the attribute key-value pairs.
+Returns a Promise that resolves when the attributes are successfully set.
+### `isFeatureEnabled(key)`
+- `key`: (required) The key of the feature flag you want to check.
+Returns a Promise that resolves to a boolean indicating whether the feature flag is enabled or not.
+### `onFeatureFlags(distinctId)`
+- `distinctId`: (optional) The distinct ID of the user.
+Returns a Promise that resolves when the feature flags are successfully reloaded.
 ## Contributing
-
 Contributions are welcome! If you find any issues or have suggestions for improvement, please create a pull request.
-
 ## License
-
 This code is open source and available under the MIT License.
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const assert = require('assert')
 const axios = require('axios')
 const version = require('./package.json').version
@@ -7,66 +6,66 @@ const version = require('./package.json').version
 class Mida {
   constructor(publicKey, options) {
     options = options || {}
-
     assert(publicKey, "You must pass your Mida project key")
-
     this.publicKey = publicKey
     this.host = 'https://api.mida.so'
     this.user_id = null
+    this.enabled_features = []
+    this.maxCacheSize = options.maxCacheSize || 50000
+    this.featureFlagCache = new Map()
   }
 
+  // A/B Testing methods
   getExperiment(experimentKey, distinctId) {
     assert(experimentKey, "You must pass your Mida experiment key")
     assert(distinctId || this.user_id, "You must pass your user distinct ID")
-
     return new Promise((resolve, reject) => {
       const data = {
         key: this.publicKey,
         experiment_key: experimentKey,
         distinct_id: distinctId || this.user_id
       }
-  
+
       const headers = {}
       if (typeof window === 'undefined') {
         headers['user-agent'] = `mida-node/${version}`
       }
-  
+
       const req = {
         method: 'POST',
         url: `${this.host}/experiment/query`,
         data,
         headers
       }
-
       axios(req)
-      .then(result => {
-        let json = result.data
-        if (json.version) resolve(json.version)
-        else resolve(null)
-      })
-      .catch((err) => {
-        if (err.response) {
-          const error = new Error(err.response.statusText)
-          reject(error)
-        }
-      })
+        .then(result => {
+          let json = result.data
+          if (json.version) resolve(json.version)
+          else resolve(null)
+        })
+        .catch((err) => {
+          if (err.response) {
+            const error = new Error(err.response.statusText)
+            reject(error)
+          }
+        })
     })
   }
 
-  setEvent(eventName, distinctId) {
-    assert(eventName, "You need to set an event name")
-    assert(distinctId || this.user_id, "You must pass your user distinct ID")
-
+  setEvent(eventName, distinctId, properties = {}) {
+    assert(eventName, "You need to set an event name");
+    assert(distinctId || this.user_id, "You must pass your user distinct ID");
     return new Promise((resolve, reject) => {
       const data = {
         key: this.publicKey,
         name: eventName,
-        distinct_id: distinctId || this.user_id
-      }
+        distinct_id: distinctId || this.user_id,
+        properties: JSON.stringify(properties)
+      };
   
-      const headers = {}
+      const headers = {};
       if (typeof window === 'undefined') {
-        headers['user-agent'] = `mida-node/${version}`
+        headers['user-agent'] = `mida-node/${version}`;
       }
   
       const req = {
@@ -74,21 +73,117 @@ class Mida {
         url: `${this.host}/experiment/event`,
         data,
         headers
+      };
+      axios(req)
+        .then(() => {
+          resolve();
+        })
+        .catch((err) => {
+          if (err.response) {
+            const error = new Error(err.response.statusText);
+            reject(error);
+          }
+        });
+    });
+  }
+
+  setAttribute(distinctId, properties = {}) {
+    assert(distinctId || this.user_id, "You must pass your user distinct ID")
+    assert(properties, "You must pass your user properties")
+    return new Promise((resolve, reject) => {
+      let data = properties
+
+      data.id = distinctId || this.user_id
+
+      const headers = {}
+      if (typeof window === 'undefined') {
+        headers['user-agent'] = `mida-node/${version}`
       }
 
+      const req = {
+        method: 'POST',
+        url: `${this.host}/track/${this.publicKey}`,
+        data,
+        headers
+      }
       axios(req)
-      .then(() => {
-        resolve()
-      })
-      .catch((err) => {
-        if (err.response) {
-          const error = new Error(err.response.statusText)
-          reject(error)
-        }
-      })
+        .then(() => {
+          resolve()
+        })
+        .catch((err) => {
+          if (err.response) {
+            const error = new Error(err.response.statusText)
+            reject(error)
+          }
+        })
     })
   }
 
+  cachedFeatureFlag() {
+    const cacheKey = `${this.publicKey}:${this.user_id}`
+    const cachedFeatures = this.featureFlagCache.get(cacheKey)
+    if (cachedFeatures) return cachedFeatures
+    return []
+  }
+
+  isFeatureEnabled(key) {
+    return new Promise((resolve) => {
+      this.enabled_features = this.cachedFeatureFlag()
+      resolve(this.enabled_features.includes(key))
+    })
+  }
+
+  onFeatureFlags(distinctId = null) {
+    return new Promise(async (resolve, reject) => {
+      const cachedItems = this.cachedFeatureFlag().length
+      try {
+        await this.reloadFeatureFlags(distinctId)
+        if (!cachedItems) resolve()
+      } catch (e) {
+        reject(e)
+      }
+      if (cachedItems) resolve()
+    })
+  }
+
+  reloadFeatureFlags(distinctId = null) {
+    return new Promise((resolve, reject) => {
+      const data = {
+        key: this.publicKey,
+        user_id: distinctId
+      }
+
+      const headers = {}
+      if (typeof window === 'undefined') {
+        headers['user-agent'] = `mida-node/${version}`
+      }
+
+      const req = {
+        method: 'POST',
+        url: `${this.host}/feature-flag`,
+        data,
+        headers
+      }
+      axios(req)
+        .then(result => {
+          this.enabled_features = result.data
+          const cacheKey = `${this.publicKey}:${this.user_id}`
+          this.featureFlagCache.set(cacheKey, result.data)
+      
+          if (this.featureFlagCache.size > this.maxCacheSize) {
+            const oldestKey = this.featureFlagCache.keys().next().value
+            this.featureFlagCache.delete(oldestKey)
+          }
+          resolve()
+        })
+        .catch((err) => {
+          if (err.response) {
+            const error = new Error(err.response.statusText)
+            reject(error)
+          }
+        })
+    })
+  }
 }
 
 module.exports = Mida
